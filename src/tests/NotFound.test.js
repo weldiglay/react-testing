0376af671d4a826808e90d6e4cb9d70ca7b8492a
weldiglay <weldiglaymachadosouza@gmail.com>
2022-05-14 10:39:10 -0300

    create test NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('Teste se a página contém o heading h2 Page requested not found 😭', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFoundTitle = screen
+    .getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+  expect(notFoundTitle).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFoundImg = screen.getAllByRole('img');
+  const imgLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(notFoundImg[1]).toHaveAttribute('src', imgLink);
+});
